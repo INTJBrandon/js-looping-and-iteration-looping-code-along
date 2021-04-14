52919b5362278a5b42c39c819ccaaf0814803958
seireitou <ausis@mail.com>
2021-04-14 04:24:38 -0500

    commit tree e060aa3dd5e269f139f474e3f2a3a3a3ddd749bf

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let new_cards = []
+    for (let i = 0; i < array.length; i++) {
+        new_cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return new_cards;
+}
+
+function countDown(n) {
+    while (n > 0) {
+        console.log(n);
+        n--
+    }
+    console.log(n)   ; 
+}
